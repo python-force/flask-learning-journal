34bd02bb4c4ff07e594b8ee678cffccce611310a
python-force <studio@pythonforce.com>
2018-10-18 23:29:38 -0400

    flake tests, forms

--- app_tests.py
+++ app_tests.py
@@ -1,9 +1,7 @@
 import unittest
 from peewee import *
-import datetime
 from models import User, Tag, Journal
 
-import app
 MODELS = [User, Tag, Journal]
 
 # use an in-memory SQLite for tests.
@@ -13,6 +11,7 @@ USER_DATA = {
     'email': 'test_0@example.com',
     'password': 'password'
 }
+
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
@@ -46,7 +45,6 @@ class BaseTestCase(unittest.TestCase):
                 email='test_1@example.com',
                 password='password'
             )
-
 
     def test_journal_creation(self):
         self.create_users()
@@ -84,155 +82,6 @@ class BaseTestCase(unittest.TestCase):
         # If we wanted, we could re-bind the models to their original
         # database here. But for tests this is probably not necessary.
 
-
-"""
-class UserModelTestCase(unittest.TestCase):
-    @staticmethod
-    def create_users(count=2):
-        for i in range(count):
-            User.create_user(
-                email='test_{}@example.com'.format(i),
-                password='password'
-            )
-
-    def test_create_user(self):
-        with test_database(TEST_DB, (User,)):
-            self.create_users()
-            self.assertEqual(User.select().count(), 2)
-            self.assertNotEqual(
-                User.select().get().password,
-                'password'
-            )
-
-    def test_create_duplicate_user(self):
-        with test_database(TEST_DB, (User,)):
-            self.create_users()
-            with self.assertRaises(ValueError):
-                User.create_user(
-                    email='test_1@example.com',
-                    password='password'
-                )
-
-
-class JournalModelTestCase(unittest.TestCase):
-    def test_journal_creation(self):
-        with test_database(TEST_DB, (User, Journal)):
-            UserModelTestCase.create_users()
-            user = User.select().get()
-            Journal.create(
-                user=user,
-                protein='chicken',
-                shell='flour',
-                cheese=False,
-                extras='Gimme some guac.'
-            )
-            taco = Taco.select().get()
-
-            self.assertEqual(
-                Taco.select().count(),
-                1
-            )
-            self.assertEqual(taco.user, user)
-
-
-
-
-class ViewTestCase(unittest.TestCase):
-    def setUp(self):
-        tacocat.app.config['TESTING'] = True
-        tacocat.app.config['WTF_CSRF_ENABLED'] = False
-        self.app = tacocat.app.test_client()
-
-
-class UserViewsTestCase(ViewTestCase):
-    def test_registration(self):
-        data = {
-            'email': 'test@example.com',
-            'password': 'password',
-            'password2': 'password'
-        }
-        with test_database(TEST_DB, (User,)):
-            rv = self.app.post(
-                '/register',
-                data=data)
-            self.assertEqual(rv.status_code, 302)
-            self.assertEqual(rv.location, 'http://localhost/')
-
-    def test_good_login(self):
-        with test_database(TEST_DB, (User,)):
-            UserModelTestCase.create_users(1)
-            rv = self.app.post('/login', data=USER_DATA)
-            self.assertEqual(rv.status_code, 302)
-            self.assertEqual(rv.location, 'http://localhost/')
-
-    def test_bad_login(self):
-        with test_database(TEST_DB, (User,)):
-            rv = self.app.post('/login', data=USER_DATA)
-            self.assertEqual(rv.status_code, 200)
-
-    def test_logout(self):
-        with test_database(TEST_DB, (User,)):
-            # Create and login the user
-            UserModelTestCase.create_users(1)
-            self.app.post('/login', data=USER_DATA)
-
-            rv = self.app.get('/logout')
-            self.assertEqual(rv.status_code, 302)
-            self.assertEqual(rv.location, 'http://localhost/')
-
-    def test_logged_out_menu(self):
-        rv = self.app.get('/')
-        self.assertIn("sign up", rv.get_data(as_text=True).lower())
-        self.assertIn("log in", rv.get_data(as_text=True).lower())
-
-    def test_logged_in_menu(self):
-        with test_database(TEST_DB, (User,)):
-            UserModelTestCase.create_users(1)
-            self.app.post('/login', data=USER_DATA)
-            rv = self.app.get('/')
-            self.assertIn("add a new taco", rv.get_data(as_text=True).lower())
-            self.assertIn("log out", rv.get_data(as_text=True).lower())
-
-
-class TacoViewsTestCase(ViewTestCase):
-    def test_empty_db(self):
-        with test_database(TEST_DB, (Taco,)):
-            rv = self.app.get('/')
-            self.assertIn("no tacos yet", rv.get_data(as_text=True).lower())
-
-    def test_taco_create(self):
-        taco_data = {
-            'protein': 'chicken',
-            'shell': 'flour',
-            'cheese': False,
-            'extras': 'Gimme some guac.'
-        }
-        with test_database(TEST_DB, (User, Taco)):
-            UserModelTestCase.create_users(1)
-            self.app.post('/login', data=USER_DATA)
-
-            taco_data['user'] = User.select().get()
-            rv = self.app.post('/taco', data=taco_data)
-            self.assertEqual(rv.status_code, 302)
-            self.assertEqual(rv.location, 'http://localhost/')
-            self.assertEqual(Taco.select().count(), 1)
-
-    def test_taco_list(self):
-        taco_data = {
-            'protein': 'chicken',
-            'shell': 'flour',
-            'cheese': False,
-            'extras': 'Gimme some guac.'
-        }
-        with test_database(TEST_DB, (User, Taco)):
-            UserModelTestCase.create_users(1)
-            taco_data['user'] = User.select().get()
-            Taco.create(**taco_data)
-
-            rv = self.app.get('/')
-            self.assertNotIn('no tacos yet', rv.get_data(as_text=True))
-            self.assertIn(taco_data['extras'], rv.get_data(as_text=True))
-"""
 
 if __name__ == '__main__':
     unittest.main()
--- forms.py
+++ forms.py
@@ -1,20 +1,25 @@
 from flask_wtf import Form
 
-from wtforms import StringField, PasswordField, TextAreaField, IntegerField, SelectMultipleField, widgets
+from wtforms import (StringField, PasswordField,
+                     IntegerField, SelectMultipleField)
 from wtforms.fields.html5 import DateField
-from wtforms.validators import ValidationError, DataRequired, Email, Regexp, Length, EqualTo
+from wtforms.validators import (ValidationError, DataRequired,
+                                Email, Length, EqualTo)
 
 from flask_pagedown.fields import PageDownField
 
-from models import User, Journal, Tag
+from models import User
+
 
 def email_exist(form, field):
     if User.select().where(User.email == field.data).exists():
         raise ValidationError('User with that email already exists.')
 
+
 def positive_value(form, field):
     if field.data < 0:
         raise ValidationError('The number must be positive and greater than 0')
+
 
 class RegistrationForm(Form):
     email = StringField(
@@ -42,8 +47,10 @@ class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 
+
 class TagForm(Form):
     title = StringField('Title', validators=[DataRequired()])
+
 
 class JournalForm(Form):
     tags = SelectMultipleField(
@@ -52,6 +59,9 @@ class JournalForm(Form):
 
     title = StringField('Title', validators=[DataRequired()])
     date = DateField('Date', validators=[DataRequired()])
-    time_spent = IntegerField('Time Spent', validators=[DataRequired(), positive_value])
-    learned = PageDownField('What I learned', validators=[DataRequired()])
-    resources = PageDownField('Resources to Remember', validators=[DataRequired()])
+    time_spent = IntegerField('Time Spent',
+                              validators=[DataRequired(), positive_value])
+    learned = PageDownField('What I learned',
+                            validators=[DataRequired()])
+    resources = PageDownField('Resources to Remember',
+                              validators=[DataRequired()])
